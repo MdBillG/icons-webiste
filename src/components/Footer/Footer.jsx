8ddbf28daeed58afb24ea4230594321522313b76
MdBillG <mdb8854@gmail.com>
2024-10-15 17:03:28 +0530

    added for footer github

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import { MoveUpRight, Triangle } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
@@ -6,12 +7,12 @@ const Footer = () => {
         <div className='border-t-[1px] '>
             <div className='flex  justify-between max-w-[68rem] mx-auto my-7'>
                 <div>Copyright © 2024 Iconarium</div>
-                <div className='flex items-baseline'>
+                <Link href="https://github.com/MdBillG/icons-webiste" className='flex items-baseline'>
                     GitHub
                     <div className='mt-1 ml-1 flex items-center'>
                         <MoveUpRight size={13} color='#929296' />
                     </div>
-                </div>
+                </Link>
                 <div className='flex gap-2 bg-black p-2 rounded-md text-white'><Triangle />
                     <div>
                         Powered by Vercel
@@ -19,6 +20,7 @@ const Footer = () => {
                 </div>
             </div>
         </div>
+
     )
 }
 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,38 +1,36 @@
-import React from 'react'
-import GitHub from './github/GitHub'
-import LightDark from './Mode/Light-Dark'
-import BrandName from './BrandName/BrandName'
-import IconsName from './BrandName/Icons'
-import PacakageName from '../Package/PacakageName'
-import Link from 'next/link'
+import React from "react";
+import GitHub from "./github/GitHub";
+import LightDark from "./Mode/Light-Dark";
+import BrandName from "./BrandName/BrandName";
+import IconsName from "./BrandName/Icons";
+import PacakageName from "../Package/PacakageName";
+import Link from "next/link";
 
 const NavBar = () => {
     return (
-        <div className='border-b border-gray-400 dark:border-gray-100 '>
-            <div className='flex justify-between gap-2 p-2 mx-[5%]'>
-                <Link href="/">  <BrandName /> </Link>
-
-
-                <div className='flex gap-3 mt-1'>
-                    <Link href="/Icons">  <IconsName /></Link>
-                    <div className='border-l-[1px] border-gray-950 dark:border-gray-100 m-1'>
-                    </div>
-                    <Link href='/packages'>
+        <div className="border-b border-gray-400 dark:border-gray-100 ">
+            <div className="flex justify-between gap-2 p-2 mx-[5%]">
+                <Link href="/">
+                    <BrandName />
+                </Link>
+                <div className="flex gap-3 mt-1">
+                    <Link href="/Icons">
+                        <IconsName />
+                    </Link>
+                    <div className="border-l-[1px] border-gray-950 dark:border-gray-100 m-1"></div>
+                    <Link href="/packages">
                         <PacakageName />
                     </Link>
-                    <div className='border-l-[1px] border-gray-950 dark:border-gray-100 m-1'>
-                    </div>
+                    <div className="border-l-[1px] border-gray-950 dark:border-gray-100 m-1"></div>
                     <LightDark />
-                    <div className='border-l-[1px] border-gray-950 dark:border-gray-100 m-1'>
-                    </div>
-                    <Link href="https://github.com/MdBillG/icons-webiste">                    <GitHub />
+                    <div className="border-l-[1px] border-gray-950 dark:border-gray-100 m-1"></div>
+                    <Link href="https://github.com/MdBillG/icons-webiste">
+                        <GitHub />
                     </Link>
                 </div>
             </div>
+        </div>
+    );
+};
 
-
-        </div >
-    )
-}
-
-export default NavBar
+export default NavBar;
