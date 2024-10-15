da2a31e68ef20a39b9e023c7be317428c0445826
MdBillG <mdb8854@gmail.com>
2024-10-15 16:56:31 +0530

    added for brandname

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,14 +4,15 @@ import LightDark from './Mode/Light-Dark'
 import BrandName from './BrandName/BrandName'
 import IconsName from './BrandName/Icons'
 import PacakageName from '../Package/PacakageName'
+import Link from 'next/link'
 
 const NavBar = () => {
     return (
         <div className='border-b border-gray-400 dark:border-gray-100 '>
             <div className='flex justify-between gap-2 p-2 mx-[5%]'>
-                <div>
-                    <BrandName />
-                </div>
+                <Link href="/">  <BrandName /> </Link>
+
+
                 <div className='flex gap-3 mt-1'>
                     <IconsName />
                     <div className='border-l-[1px] border-gray-950 dark:border-gray-100 m-1'>
